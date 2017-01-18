1a36c570ba0f9e164486dab6c6f99da79dc298b6
Rohith <gambol99@gmail.com>
2017-01-18 13:00:47 +0000

    - adding the internal aws dns name for the internal api

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,4 @@
 output "kubeapi_dns_aws"        { value = "https://${aws_elb.kubeapi.dns_name}" }
 output "kubeapi_dns"            { value = "https://${var.kubeapi_dns}.${var.public_zone_name}" }
 output "kubeapi_internal_dns"   { value = "https://${var.kubeapi_internal_dns}.${var.private_zone_name}" }
+output "kubeapi_internal_elb"   { value = "${aws_elb.kubeapi_internal.dns_name}" }
